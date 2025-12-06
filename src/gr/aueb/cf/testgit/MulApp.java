bbe35f15d9731e99e9989d664b1e6d45bf4e7e3c
pinelopi alexiadi <pinelopi.alexiadi123@gmail.com>
2025-12-06 22:40:35 +0200

    feat: add mullapp

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+
+    public static void main(String[] args) {
+
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
