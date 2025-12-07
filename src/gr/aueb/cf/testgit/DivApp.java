8203fe0d9943652242c62d24085aa7f5760b8064
pinelopi alexiadi <pinelopi.alexiadi123@gmail.com>
2025-12-07 13:21:08 +0200

    feat: add divapp

---
+++ src/gr/aueb/cf/testgit/DivApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class DivApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static int devide(int a, int b) {
+        if (b == 0) return 0;
+        return a/b;
+    }
+}
